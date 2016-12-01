2da7feba0d1b9e17a5ba1fa5b728ae432b4f3bb3
Lovely Kaur <LovelyKaur@iLovos.fritz.box>
2016-12-01 20:17:13 +0100

    startup angular2

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,34 +1,6 @@
-//jshint strict: false
-module.exports = function(config) {
-  config.set({
+/**
+ * @author: @AngularClass
+ */
 
-    basePath: './app',
-
-    files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
-      'view*/**/*.js'
-    ],
-
-    autoWatch: true,
-
-    frameworks: ['jasmine'],
-
-    browsers: ['Chrome'],
-
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-jasmine',
-      'karma-junit-reporter'
-    ],
-
-    junitReporter: {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
-
-  });
-};
+// Look in ./config for karma.conf.js
+module.exports = require('./config/karma.conf.js');
