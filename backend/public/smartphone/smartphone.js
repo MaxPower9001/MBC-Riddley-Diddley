e997da956a10bbe315365c9f296a689450c7ca39
Christof <wowcr@gmx.net>
2016-12-04 17:49:41 +0100

    debugging infos im html für sockets

--- backend/public/smartphone/smartphone.js
+++ backend/public/smartphone/smartphone.js
@@ -8,7 +8,8 @@ socket.on('connect', function(){
 socket.on('irgendwasVomServer', function(data){
     console.log("Nachricht eingetroffen, Inhalt: " + data.nachricht + " Uhrzeit: " + data.timestamp);
     var timestamp = document.createElement("p");
-    timestamp.innerText = "Nachricht eingetroffen, Inhalt: " + data.nachricht + " Nachricht erstellt: " + data.timestamp + " Nachricht eingetroffen: " + new Date().getTime();
+    diff = new Date().getTime() - data.timestamp;
+    timestamp.innerText = "Nachricht eingetroffen, Inhalt: " + data.nachricht + " Zeitdiff Nachricht erstellt/eingetroffen: " + diff;
     document.body.appendChild(timestamp);
 });
 socket.on('disconnect', function(){ console.log("Verbindung unterbrochen") });
