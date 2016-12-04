b533e04a1ab65ef40f7d3789a02936fc7951de2a
Christof <wowcr@gmx.net>
2016-12-04 17:46:51 +0100

    debugging infos im html für sockets

--- backend/public/smartphone/smartphone.js
+++ backend/public/smartphone/smartphone.js
@@ -1,6 +1,6 @@
 console.log("This is smartphone.js speaking");
 const PORT=13337;
-var socket = io('http://localhost:'+PORT);
+var socket = io('http://192.168.178.136:'+PORT);
 socket.on('connect', function(){
     console.log("wir sind verbunden");
     socket.emit("irgendwasVonDenClients", { quelle: "smartphone", timestamp: new Date().getTime() });
@@ -8,7 +8,7 @@ socket.on('connect', function(){
 socket.on('irgendwasVomServer', function(data){
     console.log("Nachricht eingetroffen, Inhalt: " + data.nachricht + " Uhrzeit: " + data.timestamp);
     var timestamp = document.createElement("p");
-    timestamp.innerText = "Nachricht eingetroffen, Inhalt: " + data.nachricht + " Uhrzeit: " + data.timestamp;
+    timestamp.innerText = "Nachricht eingetroffen, Inhalt: " + data.nachricht + " Nachricht erstellt: " + data.timestamp + " Nachricht eingetroffen: " + new Date().getTime();
     document.body.appendChild(timestamp);
 });
 socket.on('disconnect', function(){ console.log("Verbindung unterbrochen") });
