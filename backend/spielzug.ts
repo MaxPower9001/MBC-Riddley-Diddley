26cfebe9f635f915c450fd708aa25a65e8bd44ab
Christof <wowcr@gmx.net>
2017-03-02 18:38:20 +0100

    disabled Node debug variable for windows folks

--- backend/spielzug.ts
+++ backend/spielzug.ts
@@ -1,18 +1,18 @@
-import {AktionsTyp} from "../api/nachrichtentypen.interface";
-export class Spielzug {
-
-    aktionAusgefuehrt : AktionsTyp;
-    aktionGewuenscht : AktionsTyp;
-    verstricheneZeit : number;
-
-    constructor(aktionAusgefuehrt, aktionGewuenscht, verstricheneZeit) {
-        this.aktionAusgefuehrt = aktionAusgefuehrt;
-        this.aktionGewuenscht = aktionGewuenscht;
-        this.verstricheneZeit = verstricheneZeit;
-    }
-
-    toString() : string {
-        return JSON.stringify(this);
-    }
-
-}
+// import {AktionsTyp} from "../api/nachrichtentypen.interface";
+// export class Spielzug {
+//
+//     aktionAusgefuehrt : AktionsTyp;
+//     aktionGewuenscht : AktionsTyp;
+//     verstricheneZeit : number;
+//
+//     constructor(aktionAusgefuehrt, aktionGewuenscht, verstricheneZeit) {
+//         this.aktionAusgefuehrt = aktionAusgefuehrt;
+//         this.aktionGewuenscht = aktionGewuenscht;
+//         this.verstricheneZeit = verstricheneZeit;
+//     }
+//
+//     toString() : string {
+//         return JSON.stringify(this);
+//     }
+//
+// }
--- start.ts
+++ start.ts
@@ -1,17 +1,7 @@
 import {GameserverWebsocketFacade} from './backend/gameserver-websocket.facade';
-// JavaScript File
-var TX_INFO_CADS      = "[INFO CaDS][START] "
-var TX_INFO_EXPRESS   = "Modul EXPESS "
-var TX_INFO_HTTP      = "Modul HTTP "
-var TX_INFO_WEBSOCKET = "Modul WEBSOCKETS "
-var TX_INFO_ACTIVATED = "activated "
-var TX_INFO_START     = "PLAYGROUND... "
 
-///////////// Conf Service /////////////
-/* Lets start to create a new world*/
 var config            = require("./server_config.json");
 var webserverModule   = require("./backend/lib/setupWebserver");
-//var gameserverModule  = require('./gameserver_backend/gameserver.js');
 
 var serverIP    = config.server.ip   || "0.0.0.0";
 var serverPort  = config.server.port || 13337;
