3151617ae9f52a50a342aeaa8b45bb21fbe1f02d
Lovely Kaur <LovelyKaur@iLovos.fritz.box>
2017-03-04 09:42:19 +0100

    phone-end - html

--- frontend/phone-end.component.ts
+++ frontend/phone-end.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
+import { MissionControlService } from './mission-control.service';
 
 
 @Component({
-  selector: 'phone-end',
-  template: ``
+	selector: 'phone-end',
+	template: `
+		<phone-header></phone-header>
+
+		<div class="container">
+			<h3>Das Spiel ist vorbei!</h3>
+			<h4>gg Thanks</h4>
+			<h4>Bye</h4>
+		</div>
+	`
 })
 
 export class PhoneEndComponent implements OnInit {
+	
+	constructor(private missionControlService: MissionControlService) {
+	}
 
-  ngOnInit() {}
+	ngOnInit() {}
 }
